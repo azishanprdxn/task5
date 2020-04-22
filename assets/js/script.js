93a6c3b8686177ef5dc9802427e82cd02bf4d4ff
Zishan Ansari <zshan.ansari07@gmail.com>
2020-04-22 19:29:19 +0530

    Created Full Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var fetchedData, length;
 var i = 0;
 var count = 6;
 var displayedAll = false;
+document.getElementsByTagName('button')[0].onclick = getData;
 
 newRequest.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
 newRequest.send();
@@ -14,31 +15,25 @@ newRequest.onreadystatechange = function () {
     length = fetchedData.length;
     // Displays the initial 6 data
     for (i = 0; i < 6; i++) {
-      document.getElementById('data').innerHTML +=
-        "<tr>" +
-        "<td>" + fetchedData[i].id + ". " + fetchedData[i].title + "</td>" +
-        "<td>" + fetchedData[i].body + "</td>" +
-        "</td>";
+      // Calling createTable function
+      createTable(fetchedData[i].id, fetchedData[i].title, fetchedData[i].body);
     }
   }
 }
 
 function getData() {
-  displayInfo();
+  displayData();
   count = count + 6; // Increment count by 6
 }
 
-function displayInfo() {
+function displayData() {
   // Displays 6 instances of the data
   for (i = count; i < count + 6 && i < length; i++) {
-    document.getElementById('data').innerHTML +=
-      "<tr>" +
-      "<td>" + fetchedData[i].id + ". " + fetchedData[i].title + "</td>" +
-      "<td>" + fetchedData[i].body + "</td>" +
-      "</td>";
+    // Calling createTable function
+    createTable(fetchedData[i].id, fetchedData[i].title, fetchedData[i].body);
   }
 
-  // To hide the button when data has been reached
+  // hides the button when data has been reached
   if (count >= (length - 4)) {
     displayedAll = true; // Sets displayedAll to true
     if (displayedAll) {
@@ -46,4 +41,31 @@ function displayInfo() {
       document.getElementsByTagName('p')[0].style.display = "block"; // Displays the text
     }
   }
+}
+
+// Creates table
+function createTable(id, title, description) {
+  // Creation of table using DOM
+  var table = document.getElementById('data');
+  var tableRow = document.createElement('tr');
+  var tableColumn1 = document.createElement('td');
+  var tableColumn2 = document.createElement('td');
+  table.append(tableRow);
+  tableRow.append(tableColumn1);
+  tableRow.append(tableColumn2);
+  var dataId = document.createTextNode(id);
+  var dataSpace = document.createTextNode(". ");
+  var dataTitle = document.createTextNode(title);
+  var dataDescription = document.createTextNode(description);
+  tableColumn1.append(dataId);
+  tableColumn1.append(dataSpace);
+  tableColumn1.append(dataTitle);
+  tableColumn2.append(dataDescription);
+
+  // Creation of table using innerHTML
+  // table.innerHTML +=
+  //   "<tr>" +
+  //   "<td>" + id + ". " + title + "</td>" +
+  //   "<td>" + description + "</td>" +
+  //   "</td>";
 }
